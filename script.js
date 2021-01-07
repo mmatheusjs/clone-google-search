5b3c2af3ac0f1887b2bb8c76351f7cee451c4ee5
mmatheusjs <math.heenrique@live.com>
2021-01-06 23:52:00 -0300

    pesquisa e publicidade
    Paginas de pesquisa e publicidade

--- script.js
+++ script.js
@@ -3,24 +3,46 @@
 function changeColor(){
     var tema = document.getElementById('theme')
     if(tema.className == 'light'){
-        tema.classList.remove("light");        
-        tema.classList.add("dark");
+        tema.classList.remove('light');        
+        tema.classList.add('dark');
             } else{
-                tema.classList.remove("dark");
-                tema.classList.add("light");
+                tema.classList.remove('dark');
+                tema.classList.add('light');
             }
         
     var tema2 = document.getElementById('theme-header')
     if(tema2.className == 'light-header'){
-        tema2.classList.remove("light-header");
-        tema2.classList.add("dark-header");
+        tema2.classList.remove('light-header');
+        tema2.classList.add('dark-header');
         } else{
-            tema2.classList.remove("dark-header");
-            tema2.classList.add("light-header");
+            tema2.classList.remove('dark-header');
+            tema2.classList.add('light-header');
         }
-
+    
+    var tagsa = document.querySelectorAll('a');
+    for (var index = 0; index < tagsa.length; index++) {
+        var element = tagsa[index];
+        
+        if(element.classList.contains('light-a')){
+            element.classList.remove('light-a');
+            element.classList.add('dark-a');
+        } else {
+            element.classList.remove('dark-a');
+            element.classList.add('light-a');
+        }
+    }
     }
 
+
+
+
+
+
+
+    
+  
+
+     
     // function changeFont(){
     //     var color = document.getElementById('a');
     //     color.classList.toggle("dark-font");
